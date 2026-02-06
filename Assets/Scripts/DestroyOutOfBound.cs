045bcc611649dad87d97cc22ed0f703f94b227ba
TBFGunmo <94208298+TBFGunmo@users.noreply.github.com>
2026-02-06 10:51:12 +0700

    destroyOBJ

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
+    public float buffer;
+
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.z < (-Camera.main.orthographicSize - buffer) || transform.position.z > (Camera.main.orthographicSize + buffer))
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
